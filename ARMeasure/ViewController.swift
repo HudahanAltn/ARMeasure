fe8cbae9c385fb71f2a7e70df76231385226c728
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-03-03 00:52:20 +0300

    completed app

--- ARMeasure/ViewController.swift
+++ ARMeasure/ViewController.swift
@@ -10,8 +10,12 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    var dotNodes:[SCNNode] = [SCNNode] ()
+    
+    var textNode:SCNNode = SCNNode ()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +28,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,7 +59,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let hitTestResult = sceneView.hitTest(touchLocation, types:.featurePoint)
             
             if let hitResult = hitTestResult.first{
-                
+                print("touchhed")
                 addDot(at: hitResult)
             }
         }
@@ -65,7 +69,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
 extension ViewController{
     
-    func addDot(at hitResult:ARHitTestResult){ 
+    func addDot(at hitResult:ARHitTestResult){
         
         let dotGeometry = SCNSphere(radius: 0.005)
         
@@ -82,5 +86,73 @@ extension ViewController{
         
         sceneView.scene.rootNode.addChildNode(dotNode)
         
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count == 2{ // if there are two dot then you can calculate
+            
+            calculate()
+        }else if dotNodes.count < 2{//if there are less than two dots ,wait
+            
+            return
+        }else{// if there are more than 2 dots clear all and start again
+            
+            removeAll()
+        }
+        
     }
+    
+    
+    func removeAll(){// when tried to create more than  two nodes ,everything will be removed.
+        
+        for dot in dotNodes{// dots will removed
+            
+            dot.removeFromParentNode()
+        }
+        dotNodes.removeAll()
+        
+        textNode.removeFromParentNode()// text will removed
+    }
+    
+    func calculate(){//3D dimension calculation
+        
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        // distance = √ ( (X2 - X1)^2 + (Y2 - Y1)^2 + (Z2 - Z1)^2 )
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a,2) + pow(b,2) + pow(c,2))
+        
+        print( "result \(abs(distance))")
+        
+        print("calculating")
+        
+        showTextOnScreen(text: String(abs(distance)),at: end.position)
+        
+    }
+    
+    func showTextOnScreen(text:String, at position : SCNVector3){//distance text
+        
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(x: position.x, y: position.y + 0.01, z: position.z)
+        
+        textNode.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
+        
+    }
+    
+   
 }
+
+
